23c9f22cbd260529fb643fe6044aa7dbcc13a1d6
Jacob <jcmertz@outlook.com>
2025-10-02 15:58:19 -0400

    fixed problem with types and advantagekit

--- src/main/java/frc/robot/subsystems/limelight/LimelightIO.java
+++ src/main/java/frc/robot/subsystems/limelight/LimelightIO.java
@@ -1,4 +1,34 @@
 package frc.robot.subsystems.limelight;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
+import org.littletonrobotics.junction.AutoLog;
+
+import java.util.ArrayList;
+
 public interface LimelightIO {
+
+    public static enum limelightMode {
+        megaTag2, megaTag1, objectDetection
+    }
+
+    @AutoLog
+    public static class LimelightIOInputs {
+        public String limelightName = "";
+        public Pose3d robotToLimelight = new Pose3d();
+        public boolean limelightGyroInitialized = false;
+        public boolean hasTarget = false;
+//        public double targetArea = 0.0;
+//        public double targetSkew = 0.0;
+//        public double targetLatency = 0.0;
+//        public double targetShortestSide = 0.0;
+//        public double targetLongestSide = 0.0;
+//        public double targetHorizontalAngle = 0.0;
+//        public double targetVerticalAngle = 0.0;
+        public int[] tagFilter = new int[0];
+        public Pose2d targetPose = new Pose2d();
+        public Pose2d estimatedRobotPose = new Pose2d();
+    }
+
+    default void updateInputs(LimelightIOInputs inputs) {};
 }
--- src/main/java/frc/robot/subsystems/limelight/LimelightIOHardware.java
+++ src/main/java/frc/robot/subsystems/limelight/LimelightIOHardware.java
@@ -1,4 +1,267 @@
 package frc.robot.subsystems.limelight;
 
-public class LimelightIOHardware {
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.Vector;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Timer;
+import frc.robot.Constants;
+import frc.robot.Robot;
+import frc.robot.subsystems.drive.Drivetrain;
+import frc.robot.utils.LimelightHelpers;
+import org.littletonrobotics.junction.Logger;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class LimelightIOHardware implements LimelightIO{
+
+    private final Drivetrain drivetrain = Robot.drivetrain;
+
+    private final String limelightName;
+    private final Pose3d robotToLimelight;
+    private boolean doEstimation = false;
+    private boolean goodEstimationFrame = true;
+
+    private double runningTimer = 0;
+    static boolean initialEstimationsComplete = false;
+    static boolean doEstimationAll = true;
+    private int[] tagFilter = new int[0];
+    private boolean hasTarget = false;
+    private LimelightHelpers.PoseEstimate estimatedPose;
+
+    private final AprilTagFieldLayout fieldLayout = AprilTagFieldLayout.loadField(AprilTagFields.k2025ReefscapeWelded);
+
+    public LimelightIOHardware(String limelightName, Pose3d robotToLimelight) {
+        this.limelightName = limelightName;
+        this.robotToLimelight = robotToLimelight;
+    }
+
+    @Override
+    public void updateInputs(LimelightIOInputs inputs) {
+        if (LimelightHelpers.getCameraPose3d_RobotSpace(limelightName) != robotToLimelight) {
+            LimelightHelpers.setCameraPose_RobotSpace(limelightName, robotToLimelight.getX(), robotToLimelight.getY(), robotToLimelight.getZ(), Units.radiansToDegrees(robotToLimelight.getRotation().getX()), Units.radiansToDegrees(robotToLimelight.getRotation().getY()), Units.radiansToDegrees(robotToLimelight.getRotation().getZ()));
+            inputs.robotToLimelight = robotToLimelight;
+            inputs.limelightName = limelightName;
+            return;
+        }
+        hasTarget = LimelightHelpers.getTV(limelightName);
+        if (runningTimer < 15) {
+            if (hasTarget) {
+                estimatedPose = LimelightHelpers.getBotPoseEstimate_wpiBlue(limelightName);
+                if (LimelightHelpers.validPoseEstimate(estimatedPose)) {
+                    drivetrain.addVisionMeasurement(estimatedPose.pose, Timer.getFPGATimestamp(), VecBuilder.fill(0.1, 0.1, 0.2));
+                    runningTimer++;
+                }
+                return;
+            } else {
+                return;
+            }
+        } else if (runningTimer >= 15) {
+            LimelightHelpers.SetIMUMode(limelightName, 1);
+            LimelightHelpers.SetRobotOrientation(limelightName,
+                    drivetrain.getPose().getRotation().getDegrees(),
+                    0,
+                    0,
+                    0,
+                    0,
+                    0
+            );
+            initialEstimationsComplete = true;
+            inputs.limelightGyroInitialized = true;
+        }
+        if (!inputs.limelightGyroInitialized && initialEstimationsComplete && LimelightHelpers.getTV(limelightName)) {
+            LimelightHelpers.SetIMUMode(limelightName, 1);
+            LimelightHelpers.SetRobotOrientation(limelightName,
+                    drivetrain.getPose().getRotation().getDegrees(),
+                    0,
+                    0,
+                    0,
+                    0,
+                    0
+            );
+            estimatedPose = LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2(limelightName);
+            LimelightHelpers.SetIMUMode(limelightName, 2);
+            inputs.limelightGyroInitialized = true;
+        } else if (inputs.limelightGyroInitialized && initialEstimationsComplete && LimelightHelpers.getTV(limelightName)) {
+             estimatedPose = LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2(limelightName);
+        }
+
+        if (Math.abs(drivetrain.getPigeon2().getAngularVelocityZDevice().getValueAsDouble()) > 720) {
+            goodEstimationFrame = false;
+        }
+        if (estimatedPose.tagCount == 0) {
+            goodEstimationFrame = false;
+        }
+
+        if (goodEstimationFrame && doEstimation && doEstimationAll) {
+            drivetrain.addVisionMeasurement(estimatedPose.pose);
+        }
+
+        inputs.hasTarget = hasTarget;
+        inputs.estimatedRobotPose = estimatedPose.pose;
+        inputs.tagFilter = tagFilter;
+        inputs.targetPose = LimelightHelpers.toPose2D(LimelightHelpers.getTargetPose_RobotSpace(limelightName));
+
+    }
+
+    public void resetIMU() {
+        doEstimation = false;
+
+        LimelightHelpers.SetIMUMode(limelightName, 1);
+        LimelightHelpers.SetRobotOrientation(limelightName,
+                Robot.drivetrain.getPose().getRotation().getDegrees(),
+                0,
+                0,
+                0,
+                0,
+                0
+        );
+        LimelightHelpers.SetIMUMode(limelightName, 2);
+
+        doEstimation = true;
+    }
+
+    public void setTagFilter(int[] ids) {
+        tagFilter = ids;
+        LimelightHelpers.SetFiducialIDFiltersOverride(limelightName, ids);
+    }
+
+    public void resetTagFilter() {
+        tagFilter = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22};
+        LimelightHelpers.SetFiducialIDFiltersOverride(limelightName, new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22});
+    }
+
+    public double getXFromTag() {
+        return LimelightHelpers.getTargetPose_RobotSpace(limelightName)[0];
+    }
+
+    public double getYFromTag() {
+        return LimelightHelpers.getTargetPose_RobotSpace(limelightName)[1];
+    }
+
+    public double getZFromTag() {
+        return LimelightHelpers.getTargetPose_RobotSpace(limelightName)[2];
+    }
+
+    public boolean tagIsVisible() {
+        return LimelightHelpers.getTV(limelightName);
+    }
+
+    public double getTX() {
+        return LimelightHelpers.getTX(limelightName);
+    }
+
+    public double getTY() {
+        return LimelightHelpers.getTY(limelightName);
+    }
+
+    public void turnOffAprilTags() {
+        doEstimation = false;
+    }
+
+    public void turnOnAprilTags() {
+        doEstimation = true;
+    }
+
+    public void turnOffAllAprilTagsImplementation() {
+        doEstimationAll = false;
+    }
+
+    public void turnOnAllAprilTagsImplementation() {
+        doEstimationAll = true;
+    }
+
+    public Rotation2d getTagAngleImplementation(int tagID) {
+        return fieldLayout.getTagPose(tagID).get().toPose2d().getRotation();
+    }
+
+    public Pose2d getTagPoseImplementation(int tagID) {
+        return fieldLayout.getTagPose(tagID).get().toPose2d();
+    }
+
+    public double getNearestReefAngleImplementation() {
+        if (Robot.getAlliance() == DriverStation.Alliance.Blue) {
+            List<Integer> nearestTags = Arrays.asList(Constants.ReefTagIDs.blue);
+
+            nearestTags.sort(new Comparator<Integer>() {
+
+                Drivetrain drivetrain = Robot.drivetrain;
+
+                @Override
+                public int compare(Integer tag1, Integer tag2) {
+                    double distanceToTag1 = Limelight.getTagPose(tag1).getTranslation().getDistance(drivetrain.getPose().getTranslation());
+                    double distanceToTag2 = Limelight.getTagPose(tag2).getTranslation().getDistance(drivetrain.getPose().getTranslation());
+
+                    return (int) Math.floor(distanceToTag1 - distanceToTag2);
+                }
+            });
+
+            return Limelight.getTagAngle(nearestTags.get(0)).plus(Rotation2d.fromDegrees(180)).getDegrees();
+
+        } else {
+            List<Integer> nearestTags = Arrays.asList(Constants.ReefTagIDs.red);
+
+            nearestTags.sort(new Comparator<Integer>() {
+
+                Drivetrain drivetrain = Robot.drivetrain;
+
+                @Override
+                public int compare(Integer tag1, Integer tag2) {
+                    double distanceToTag1 = Limelight.getTagPose(tag1).getTranslation().getDistance(drivetrain.getPose().getTranslation());
+                    double distanceToTag2 = Limelight.getTagPose(tag2).getTranslation().getDistance(drivetrain.getPose().getTranslation());
+
+                    return (int) Math.floor(distanceToTag1 - distanceToTag2);
+                }
+            });
+
+            return Limelight.getTagAngle(nearestTags.get(0)).plus(Rotation2d.fromDegrees(180)).getDegrees();
+        }
+    }
+
+    public int getNearestReefTagImplementation() {
+        if (Robot.getAlliance() == DriverStation.Alliance.Blue) {
+            List<Integer> nearestTags = Arrays.asList(Constants.ReefTagIDs.blue);
+
+            nearestTags.sort(new Comparator<Integer>() {
+
+                Drivetrain drivetrain = Robot.drivetrain;
+
+                @Override
+                public int compare(Integer tag1, Integer tag2) {
+                    double distanceToTag1 = Limelight.getTagPose(tag1).getTranslation().getDistance(drivetrain.getPose().getTranslation());
+                    double distanceToTag2 = Limelight.getTagPose(tag2).getTranslation().getDistance(drivetrain.getPose().getTranslation());
+
+                    return (int) Math.floor(distanceToTag1 - distanceToTag2);
+                }
+            });
+
+            return nearestTags.get(0);
+
+        } else {
+            List<Integer> nearestTags = Arrays.asList(Constants.ReefTagIDs.red);
+
+            nearestTags.sort(new Comparator<Integer>() {
+
+                Drivetrain drivetrain = Robot.drivetrain;
+
+                @Override
+                public int compare(Integer tag1, Integer tag2) {
+                    double distanceToTag1 = Limelight.getTagPose(tag1).getTranslation().getDistance(drivetrain.getPose().getTranslation());
+                    double distanceToTag2 = Limelight.getTagPose(tag2).getTranslation().getDistance(drivetrain.getPose().getTranslation());
+
+                    return (int) Math.floor(distanceToTag1 - distanceToTag2);
+                }
+            });
+
+            return nearestTags.get(0);
+        }
+    }
 }
